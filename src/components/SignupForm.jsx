36fb45c5cdcbe4e16f0ca33900c8f9494020193e
Asad Khan <asadkhan6192@gmail.com>
2022-11-24 20:20:59 +0500

    Add validation logic to SignupForm

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -3,45 +3,127 @@ import GoogleButton from "./GoogleButton";
 import LoginAndSignUpLayout from "./LoginAndSignUpLayout";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { createAccountUsingEmail, usernameAvailable, emailNotRegistered } from "../firebase";
 import "../styles/SignupForm.css";
 
 const SignupForm = () => {
-    const [data, setData] = useState("");
+    const [data, setData] = useState({
+        email: "",
+        username: "",
+        password: ""
+    });
+
     const [continueClicked, setContinueClicked] = useState(false);
+    const [error, setError] = useState({
+        usernameTaken: false,
+        passwordTooShort: false,
+        emailAlreadyRegistered: false
+    });
 
     const handleInput = (e) => {
         setData({
             ...data,
             [e.target.name]: e.target.value
         });
+        if (error.emailAlreadyRegistered && e.target.name === "email") {
+            setError({
+                ...error,
+                emailAlreadyRegistered: false
+            })
+        }
     };
 
-    const handleFormSubmit = (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
-
+        if (!(Object.values(error).some(error => error))) {
+            if (data.password < 8 && !(await usernameAvailable(data.username))) {
+                setError({
+                    ...error,
+                    usernameTaken: true,
+                    passwordTooShort: true
+                })
+            }
+            else if (data.password < 8) {
+                setError({
+                    ...error,
+                    passwordTooShort: true
+                });
+            }
+            else if (!(await usernameAvailable(data.username))) {
+                setError({
+                    ...error,
+                    usernameTaken: true
+                })
+            }
+            else {
+                createAccountUsingEmail(data);
+            }
+        }
     };
 
-    const handleContinue = () => {
-        setContinueClicked(true);
+    const handleContinue = async (e) => {
+        e.preventDefault();
+        if (await emailNotRegistered(e.target.email.value)) {
+            setContinueClicked(true);
+        }
+        else {
+            setError({
+                ...error,
+                emailAlreadyRegistered: true,
+            })
+        }
     };
 
     const handleBack = () => {
         setContinueClicked(false);
-    }
+    };
+
+    const handleUsernameBlur = async (e) => {
+        if (!(await usernameAvailable(e.target.value))) {
+            setError({
+                ...error,
+                usernameTaken: true
+            })
+        }
+        else {
+            setError({
+                ...error,
+                usernameTaken: false
+            });
+        }
+    };
+
+    const handlePasswordBlur = (e) => {
+        if (e.target.value.length < 8) {
+            setError({
+                ...error,
+                passwordTooShort: true
+            })
+        }
+        else {
+            setError({
+                ...error,
+                passwordTooShort: false
+            });
+        }
+    };
+
+    const submitBtnDisabled = (Object.values(error).some(error => error));
 
     if (!continueClicked) {
         return (
             <LoginAndSignUpLayout>
                 <h4>Sign up</h4>
-                <form className="login-form" onSubmit={handleFormSubmit}>
+                <form className="login-form" onSubmit={!error.emailAlreadyRegistered ? handleContinue : null}>
                     <p className="user-agreement smaller">
                         By continuing, you are setting up a Reddit account and agree to our
                         User Agreement and Privacy Policy.
                     </p>
                     <GoogleButton />
                     <AppleButton />
-                    <input name="email" type="text" id="email-input" placeholder="Email" value={data.email} onChange={handleInput} />
-                    <button className="submit-btn" onClick={handleContinue}>Continue</button>
+                    <input className={`form-input ${error.emailAlreadyRegistered ? "form-input__error" : ""}`} name="email" type="email" placeholder="Email" value={data.email} onChange={handleInput} />
+                    {error.emailAlreadyRegistered && <div className="error-message">This email is already taken</div>}
+                    <button className="submit-btn" disabled={error.emailAlreadyRegistered}>Continue</button>
                     <p className="smaller">Already a redditor? <Link to="/login">Log in</Link> </p>
                 </form>
             </LoginAndSignUpLayout>
@@ -56,13 +138,15 @@ const SignupForm = () => {
             </header>
             <main className="sign-up-form__main">
                 <form className="sign-up-form">
-                    <input className="form-input" name="username" type="text" placeholder="Choose a username" />
-                    <input className="form-input" name="password" type="password" placeholder="Password" />
+                    <input className={`form-input ${error.usernameTaken ? "form-input__error" : ""}`} name="username" type="text" placeholder="Choose a username" value={data.username} onChange={handleInput} onBlur={handleUsernameBlur} />
+                    {error.usernameTaken && <div className="error-message">That username is already taken</div>}
+                    <input className={`form-input ${error.passwordTooShort ? "form-input__error" : ""}`} name="password" type="password" placeholder="Password" value={data.password} onChange={handleInput} onBlur={handlePasswordBlur} />
+                    {error.passwordTooShort && <div className="error-message">Password must be at least 8 characters long</div>}
                 </form>
             </main>
             <footer className="sign-up-form__footer">
                 <Link to="/signup" className="back-link" onClick={handleBack}>Back</Link>
-                <button className="primary-btn">Sign up</button>
+                <button className="primary-btn" type="submit" onClick={!submitBtnDisabled ? handleFormSubmit : null} disabled={submitBtnDisabled}>Sign up</button>
             </footer>
         </div>
     )
---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { addDoc, collection, getDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const createAccountUsingEmail = async ({ email, password, username }) => {
+    try {
+        const userCreds = await createUserWithEmailAndPassword(auth, email, password);
+        await addDoc(collection(db, "Users"), { username: username, email: userCreds.user.email })
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
+const usernameAvailable = async (username) => {
+    const usersRef = collection(db, "Users");
+    const q = query(usersRef, where("username", "==", username));
+    const snapshot = await getDocs(q);
+    return snapshot.empty || false;
+};
+
+const emailNotRegistered = async (email) => {
+    const usersRef = collection(db, "Users");
+    const q = query(usersRef, where("email", "==", email));
+    const snapshot = await getDocs(q);
+    return snapshot.empty || false;
+}
+
+export { createAccountUsingEmail, usernameAvailable, emailNotRegistered };
