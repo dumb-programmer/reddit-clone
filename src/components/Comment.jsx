66e50ef7d517ffc99396d5eab2f51ff40c87d3e9
Asad Khan <asadkhan6192@gmail.com>
2023-02-19 21:55:31 +0500

    Add comment minimize/maximize functionality

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -12,11 +12,13 @@ import {
   getComments,
   subscribeToComments,
 } from "../firebase";
+import MaximizeIcon from "./MaximizeIcon";
 
 const Comment = ({ comment, saved, setToastText, showToast }) => {
   const [edit, setEdit] = useState(false);
   const [reply, setReply] = useState(false);
   const [replies, setReplies] = useState(null);
+  const [minimize, setMinimize] = useState(false);
 
   const data = comment.data();
 
@@ -40,59 +42,108 @@ const Comment = ({ comment, saved, setToastText, showToast }) => {
     };
   }, [data]);
 
-  return (
-    <div className="comment-container">
-      <div className="comment">
+  if (minimize) {
+    return (
+      <div
+        className="comment"
+        style={{ display: "flex", alignItems: "center", gap: "0.7rem" }}
+      >
+        <button onClick={() => setMinimize(false)}>
+          <MaximizeIcon height={20} width={20} fill="#ffff" stroke="#0079d3" />
+        </button>
         <img
           src="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_7.png"
           alt="avatar"
           className="avatar"
         />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            paddingLeft: 10,
-            flex: 2,
-          }}
-        >
-          <div className="comment-header">
-            <span>{data.author}</span>
-            <span
-              style={{
-                color: "#a4a7a8",
-                marginLeft: 10,
-              }}
-            >
-              {data.createdOn > 0
-                ? getRelativeDateTime(data.createdOn.toMillis())
-                : "Just Now"}
-            </span>
-            {data.editedOn && (
-              <>
-                <span
-                  style={{
-                    color: "#a4a7a8",
-                    marginLeft: 10,
-                  }}
-                >
-                  .{" "}
-                </span>
+        <div className="comment-header">
+          <span>{data.author}</span>
+          <span
+            style={{
+              color: "#a4a7a8",
+              marginLeft: 10,
+            }}
+          >
+            {data.createdOn > 0
+              ? getRelativeDateTime(data.createdOn.toMillis())
+              : "Just Now"}
+          </span>
+          {data.editedOn && (
+            <>
+              <span
+                style={{
+                  color: "#a4a7a8",
+                  marginLeft: 10,
+                }}
+              >
+                .{" "}
+              </span>
 
-                <span
-                  style={{
-                    color: "#a4a7a8",
-                    marginLeft: 10,
-                  }}
-                >
-                  edited{" "}
-                  {data.editedOn > 0
-                    ? getRelativeDateTime(data.editedOn.toMillis())
-                    : "Just Now"}
-                </span>
-              </>
-            )}
-          </div>
+              <span
+                style={{
+                  color: "#a4a7a8",
+                  marginLeft: 10,
+                }}
+              >
+                edited{" "}
+                {data.editedOn > 0
+                  ? getRelativeDateTime(data.editedOn.toMillis())
+                  : "Just Now"}
+              </span>
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="comment">
+      <div className="comment-sidebar">
+        <img
+          src="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_7.png"
+          alt="avatar"
+          className="avatar"
+        />
+        <div className="thread-line" onClick={() => setMinimize(true)}></div>
+      </div>
+      <div className="comment-main">
+        <div className="comment-body">
+          <span>{data.author}</span>
+          <span
+            style={{
+              color: "#a4a7a8",
+              marginLeft: 10,
+            }}
+          >
+            {data.createdOn > 0
+              ? getRelativeDateTime(data.createdOn.toMillis())
+              : "Just Now"}
+          </span>
+          {data.editedOn && (
+            <>
+              <span
+                style={{
+                  color: "#a4a7a8",
+                  marginLeft: 10,
+                }}
+              >
+                .{" "}
+              </span>
+
+              <span
+                style={{
+                  color: "#a4a7a8",
+                  marginLeft: 10,
+                }}
+              >
+                edited{" "}
+                {data.editedOn > 0
+                  ? getRelativeDateTime(data.editedOn.toMillis())
+                  : "Just Now"}
+              </span>
+            </>
+          )}
           {!edit ? (
             <p>{data.comment}</p>
           ) : (
@@ -106,57 +157,57 @@ const Comment = ({ comment, saved, setToastText, showToast }) => {
             />
           )}
         </div>
-      </div>
-      {!edit && (
-        <div className="comment-footer">
-          <Vote data={data} type="comment" />
-          <button
-            className="reply-btn"
-            onClick={() => setReply((prev) => !prev)}
-          >
-            <MessageIcon height={24} width={24} stroke="black" />
-            <span>Reply</span>
-          </button>
-          <ShowMore
-            id={data.id}
-            confirmationText="Are you sure you want to delete your comment?"
-            confirmationHeader="Delete comment"
-            onEdit={() => setEdit(true)}
-            isSaved={saved && saved.includes(comment.data().id)}
-            isOwner={localStorage.getItem("username") === data?.author}
-            handleDelete={async () => {
-              await deleteComment(comment.ref);
-            }}
-            context="comment"
-            showToast={showToast}
-            setToastText={setToastText}
-          />
-        </div>
-      )}
-      <div style={{ marginLeft: 60, paddingRight: 10 }}>
-        {reply && (
-          <CommentBox
-            primaryCaption="Reply"
-            onSubmit={async (comment) => {
-              await createComment(
-                comment,
-                localStorage.getItem("username"),
-                data?.id
-              );
-            }}
-            onCancel={() => setReply(false)}
-          />
-        )}
-        {replies &&
-          replies.map((replies) => (
-            <Comment
-              key={replies.data().id}
-              comment={replies}
-              saved={saved}
+        {!edit && (
+          <div className="comment-footer">
+            <Vote data={data} type="comment" />
+            <button
+              className="reply-btn"
+              onClick={() => setReply((prev) => !prev)}
+            >
+              <MessageIcon height={24} width={24} stroke="black" />
+              <span>Reply</span>
+            </button>
+            <ShowMore
+              id={data.id}
+              confirmationText="Are you sure you want to delete your comment?"
+              confirmationHeader="Delete comment"
+              onEdit={() => setEdit(true)}
+              isSaved={saved && saved.includes(comment.data().id)}
+              isOwner={localStorage.getItem("username") === data?.author}
+              handleDelete={async () => {
+                await deleteComment(comment.ref);
+              }}
+              context="comment"
               showToast={showToast}
               setToastText={setToastText}
             />
-          ))}
+          </div>
+        )}
+        <div className="replies">
+          {reply && (
+            <CommentBox
+              primaryCaption="Reply"
+              onSubmit={async (comment) => {
+                await createComment(
+                  comment,
+                  localStorage.getItem("username"),
+                  data?.id
+                );
+              }}
+              onCancel={() => setReply(false)}
+            />
+          )}
+          {replies &&
+            replies.map((replies) => (
+              <Comment
+                key={replies.data().id}
+                comment={replies}
+                saved={saved}
+                showToast={showToast}
+                setToastText={setToastText}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/MaximizeIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MaximizeIcon = ({ height, width, fill, stroke }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-width="1.3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      style={{ height, width, fill, stroke, transform: "rotate(90deg)" }}
+    >
+      <polyline points="15 3 21 3 21 9"></polyline>
+      <polyline points="9 21 3 21 3 15"></polyline>
+      <line x1="21" y1="3" x2="14" y2="10"></line>
+      <line x1="3" y1="21" x2="10" y2="14"></line>
+    </svg>
+  );
+};
+
+export default MaximizeIcon;
